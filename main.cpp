1748a6d85a41e728deb6c66c74a4a9f90f13db26
Ceyhun Cakir <ceyhuncakir@live.nl>
2022-02-20 22:36:40 +0100

    morgen fix
    morgen fix

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 #include <algorithm>
 #include <cmath>
 #include <sstream>
+#include <string>
 
 template <typename T>
-std::vector<T> bucket_sort(std::vector<T> array, int k) {
+std::vector<T> bucket_sort(std::vector<T> array, int k, int i) {
 
 
   // initialiseren we een lijst met vectoren er in waar integers in zitten.
@@ -22,24 +23,32 @@ std::vector<T> bucket_sort(std::vector<T> array, int k) {
 
   // O(n)
   // we itereren door de lijst met getallen die gesorteerd moeten worden.
-  for(int i = 0; i < array.size(); i++) {
 
+  for(int idx = 0; idx < array.size(); idx++) {
+                                                                                                        //3    2        2    2
+    // verwijder als eerst sws de - van de getallen die negatief zijn zodat de waardes gelijk blijven. (-159, 159) -> (159, 159). zodat die niet de minus neemt als idx
+    // check of het mogelijk is dat voor een gegeven array[idx] de idx 3 heeft in het string.
+    // neem de length van de string characters voorbeeld: 232 = (2) || i = 2 || 2 - 3 = 1 || i - 2 - i
 
     // we maken een ostringstream aan voor een string buffer waar een sequence aan chars opgeslagen worden.
     std::ostringstream ss;
     // we zetten de datatype in de string buffer
-    ss << array[i];
+    ss << array[idx];
     // we verranderen uiteindelijk de datatype naar een string
     std::string string_integer = ss.str();
 
+    string_integer.erase(std::remove(string_integer.begin(), string_integer.end(), '-'), string_integer.end());
+
+    // de waarde die van de string lengte er af word getrokken om de juiste positie te krijgen
     // we pakken van de hele string de laatse char.
-    char string_integer_last_char = string_integer.back();
+
+    char string_integer_last_char = string_integer[i - 1];
 
     // we verranderen de char terug naar een int door een int declraratie te geven en ook - 48 te gebruiken aangezien ascii chars op nummer 48 starten.
     int last_digit = (int) string_integer_last_char - 48;
 
     // we voegen de getallen naar de correspondente buckets
-    buckets[last_digit].push_back(array[i]);
+    buckets[last_digit].push_back(array[idx]);
   }
 
   // we gebruiken de clear functie om de nieuwe gesorteerde waardes er in te zetten.
@@ -54,7 +63,6 @@ std::vector<T> bucket_sort(std::vector<T> array, int k) {
       array.push_back(buckets[i][idx]);
     }
   }
-
   // we returnen de gesorteerde originele array terug.
   return array;
 }
@@ -83,24 +91,26 @@ int main() {
     array.push_back(random_generated_integer);
   }
 
-  // vraag aan docent hoe je de vector array als een T kan krijgen
+  int max_itt = *max_element(array.begin(), array.end());
 
-  array.push_back(-1.5);
-  array.push_back(-0.9);
-  array.push_back(1.2);
-  array.push_back(0.4);
+  std::ostringstream ss;
+  ss << max_itt;
+  std::string string_integer = ss.str();
 
-  std::vector<float> sorted_array = bucket_sort(array, k);
+  std::cout << string_integer.length() << std::endl;
+
+  std::vector<float> sorted_array = bucket_sort(array, k, string_integer.length());
 
   std::cout << "given array - ";
 
   for(const auto elem : array) { std::cout << elem << " | "; }
   std::cout << std::endl;
 
-  std::cout << "sorted array - ";
+  std::cout << "sorted array 3 - ";
 
   for(const auto elem : sorted_array) { std::cout << elem << " | "; }
   std::cout << std::endl;
+
 
   return 0;
 }
