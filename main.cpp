2c31ef841bb62b212038c2e9509ce6959826fa95
Ceyhun Cakir <ceyhuncakir@live.nl>
2022-02-23 17:43:55 +0100

    update finished
    update finished

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include <cmath>
 #include <sstream>
 #include <string>
+#include <chrono>
+
+using namespace std::chrono;
 
 template <typename T>
 std::vector<T> bucket_sort(std::vector<T> array, int max_elem) {
@@ -43,17 +46,206 @@ void test_bucket_sort(std::vector<float> sorted_array, std::vector<float> unsort
   // build in functie om te bevestigen dat de algoritme werkt
   std::sort(unsorted_array.begin(), unsorted_array.end());
 
-  std::cout << "sorted array - ";
-
-  for(const auto elem : unsorted_array) { std::cout << elem << " | "; }
-  std::cout << std::endl;
-
   // test of mijn uitkomsten de zelfde zijn als die van c++
   if(unsorted_array == sorted_array) {
-    std::cout << "de uitkomst van de eigen gemaakte bucket sort tegenover de sorting algoritme van c++: True";
+    std::cout << " " << std::endl;
+    std::cout << "-----------Algorithm sorted validation checkt---------" << std::endl;
+    std::cout << "de uitkomst van de eigen gemaakte bucket sort tegenover de sorting algoritme van c++: True" << std::endl;
   } else {
-    std::cout << "de uitkomst van de eigen gemaakte bucket sort tegenover de sorting algoritme van c++: False";
+    std::cout << " " << std::endl;
+    std::cout << "-----------Algorithm sorted validation check---------" << std::endl;
+    std::cout << "----------Worst case scenario of the bucket sort---------" << std::endl;
+    std::cout << "de uitkomst van de eigen gemaakte bucket sort tegenover de sorting algoritme van c++: False" << std::endl;
   }
+}
+
+void test_bucket_sort_worst_case() {
+
+  // de vectoren met de ongesorteerde waardes
+  std::vector<float> negative_numbers = {-10, -20, -400, -4000, -15000};
+  std::vector<float> positive_numbers = {110000, 20000, 15000, 4000, 400};
+  std::vector<float> unsorted = {-10, -20, -400, -4000, -15000, 110000, 20000, 15000, 4000, 400};
+
+  // hier worden de max element en min element gepakt van de vectors
+  int max_negative = *min_element(negative_numbers.begin(), negative_numbers.end());
+  int max_positive = *max_element(positive_numbers.begin(), positive_numbers.end());
+
+  // de negatieve getallen worden omgebouwt naar een string
+  std::ostringstream ss_neg;
+  ss_neg << abs(max_negative);
+  std::string string_integer_neg = ss_neg.str();
+  int max_elem_neg = string_integer_neg.length();
+
+  // de positieve getallen worden omgebouwt naar een string
+  std::ostringstream ss_pos;
+  ss_pos << max_positive;
+  std::string string_integer_pos = ss_pos.str();
+  int max_elem_pos = string_integer_pos.length();
+
+  // counter word bijgehouden voor de run time
+  auto start = high_resolution_clock::now();
+
+  // negatieve getallen worden gesorteerd
+  std::vector<float> sorted_array_neg = bucket_sort(negative_numbers, max_elem_neg - 1);
+  // gesorte negatieve getallen worden gereversed zodat het goed komt te staan later bij de merge
+  std::reverse(sorted_array_neg.begin(), sorted_array_neg.end());
+
+  // positieve getallen worden gesorteerd
+  std::vector<float> sorted_array_pos = bucket_sort(positive_numbers, max_elem_pos - 1);
+
+  // counter stopt met bijgehouden voor de run time
+  auto stop = high_resolution_clock::now();
+
+  // positieve gettalen worden bijgevoegd bij de negatieve getallen.
+  sorted_array_neg.insert(sorted_array_neg.end(), sorted_array_pos.begin(), sorted_array_pos.end());
+
+  std::cout << " " << std::endl;
+
+  std::cout << "----------Worst case scenario of the bucket sort---------" << std::endl;
+
+  std::cout << " " << std::endl;
+
+  std::cout << "given test array - ";
+
+  for(const auto elem : unsorted) { std::cout << elem << " | "; }
+  std::cout << std::endl;
+
+  std::cout << "sorted test array - ";
+
+  for(const auto elem : sorted_array_neg) { std::cout << elem << " | "; }
+  std::cout << std::endl;
+
+  // duration word hier gecalculeert.
+  auto duration = duration_cast<microseconds>(stop - start);
+
+  std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
+
+}
+
+void test_bucket_sort_average_case() {
+
+  // de vectoren met de ongesorteerde waardes
+  std::vector<float> negative_numbers = {-63211, -72690, -17285, -53096};
+  std::vector<float> positive_numbers = {27079, 88632, 10250, 25437, 85871, 13023};
+  std::vector<float> unsorted = {-63211, 27079, 88632, -72690, 10250, 25437, -17285, 85871, -53096, 13023};
+
+  // hier worden de max element en min element gepakt van de vectors
+  int max_negative = *min_element(negative_numbers.begin(), negative_numbers.end());
+  int max_positive = *max_element(positive_numbers.begin(), positive_numbers.end());
+
+  // de negatieve getallen worden omgebouwt naar een string
+  std::ostringstream ss_neg;
+  ss_neg << abs(max_negative);
+  std::string string_integer_neg = ss_neg.str();
+  int max_elem_neg = string_integer_neg.length();
+
+  // de positieve getallen worden omgebouwt naar een string
+  std::ostringstream ss_pos;
+  ss_pos << max_positive;
+  std::string string_integer_pos = ss_pos.str();
+  int max_elem_pos = string_integer_pos.length();
+
+  // counter word bijgehouden voor de run time
+  auto start = high_resolution_clock::now();
+
+  // negatieve getallen worden gesorteerd
+  std::vector<float> sorted_array_neg = bucket_sort(negative_numbers, max_elem_neg - 1);
+  // gesorte negatieve getallen worden gereversed zodat het goed komt te staan later bij de merge
+  std::reverse(sorted_array_neg.begin(), sorted_array_neg.end());
+
+  // positieve getallen worden gesorteerd
+  std::vector<float> sorted_array_pos = bucket_sort(positive_numbers, max_elem_pos - 1);
+
+  // counter stopt met bijgehouden voor de run time
+  auto stop = high_resolution_clock::now();
+
+  // positieve gettalen worden bijgevoegd bij de negatieve getallen.
+  sorted_array_neg.insert(sorted_array_neg.end(), sorted_array_pos.begin(), sorted_array_pos.end());
+
+  std::cout << " " << std::endl;
+
+  std::cout << "----------Average case scenario of the bucket sort---------" << std::endl;
+
+  std::cout << " " << std::endl;
+
+  std::cout << "given test array - ";
+
+  for(const auto elem : unsorted) { std::cout << elem << " | "; }
+  std::cout << std::endl;
+
+  std::cout << "sorted test array - ";
+
+  for(const auto elem : sorted_array_neg) { std::cout << elem << " | "; }
+  std::cout << std::endl;
+
+  // duration word hier gecalculeert.
+  auto duration = duration_cast<microseconds>(stop - start);
+
+  std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
+
+}
+
+void test_bucket_sort_best_case() {
+
+  // de vectoren met de ongesorteerde waardes
+  std::vector<float> negative_numbers = {-91243, -87564, -68757, -2186,};
+  std::vector<float> positive_numbers = {14406, 20622, 23253, 31020, 76913, 89908};
+  std::vector<float> unsorted = {-91243, -87564, -68757, -2186, 14406, 20622, 23253, 31020, 76913, 89908};
+
+  // hier worden de max element en min element gepakt van de vectors
+  int max_negative = *min_element(negative_numbers.begin(), negative_numbers.end());
+  int max_positive = *max_element(positive_numbers.begin(), positive_numbers.end());
+
+  // de negatieve getallen worden omgebouwt naar een string
+  std::ostringstream ss_neg;
+  ss_neg << abs(max_negative);
+  std::string string_integer_neg = ss_neg.str();
+  int max_elem_neg = string_integer_neg.length();
+
+  // de positieve getallen worden omgebouwt naar een string
+  std::ostringstream ss_pos;
+  ss_pos << max_positive;
+  std::string string_integer_pos = ss_pos.str();
+  int max_elem_pos = string_integer_pos.length();
+
+  // counter word bijgehouden voor de run time
+  auto start = high_resolution_clock::now();
+
+  // negatieve getallen worden gesorteerd
+  std::vector<float> sorted_array_neg = bucket_sort(negative_numbers, max_elem_neg - 1);
+  // gesorte negatieve getallen worden gereversed zodat het goed komt te staan later bij de merge
+  std::reverse(sorted_array_neg.begin(), sorted_array_neg.end());
+
+  // positieve getallen worden gesorteerd
+  std::vector<float> sorted_array_pos = bucket_sort(positive_numbers, max_elem_pos - 1);
+
+  // counter stopt met bijgehouden voor de run time
+  auto stop = high_resolution_clock::now();
+
+  // positieve gettalen worden bijgevoegd bij de negatieve getallen.
+  sorted_array_neg.insert(sorted_array_neg.end(), sorted_array_pos.begin(), sorted_array_pos.end());
+
+  std::cout << " " << std::endl;
+
+  std::cout << "----------Best case scenario of the bucket sort---------" << std::endl;
+
+  std::cout << " " << std::endl;
+
+  std::cout << "given test array - ";
+
+  for(const auto elem : unsorted) { std::cout << elem << " | "; }
+  std::cout << std::endl;
+
+  std::cout << "sorted test array - ";
+
+  for(const auto elem : sorted_array_neg) { std::cout << elem << " | "; }
+  std::cout << std::endl;
+
+  // duration word hier gecalculeert.
+  auto duration = duration_cast<microseconds>(stop - start);
+
+  std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
+
 }
 
 int main() {
@@ -97,6 +289,8 @@ int main() {
     }
   }
 
+  auto start = high_resolution_clock::now();
+
   // word er gekeken of de lijst van de negatieve nummers niet empty is
   if(!negative_numbers.empty()) {
     int max_negative = *min_element(negative_numbers.begin(), negative_numbers.end());
@@ -123,7 +317,13 @@ int main() {
     sorted_array_pos = bucket_sort(positive_numbers, max_elem_pos - 1);
   }
 
+  auto stop = high_resolution_clock::now();
+
   sorted_array_neg.insert(sorted_array_neg.end(), sorted_array_pos.begin(), sorted_array_pos.end());
+
+  std::cout << " " << std::endl;
+
+  std::cout << "----------------User inputted outcome---------" << std::endl;
 
   std::cout << "given array - ";
 
@@ -135,7 +335,17 @@ int main() {
   for(const auto elem : sorted_array_neg) { std::cout << elem << " | "; }
   std::cout << std::endl;
 
-  //test_bucket_sort(sorted_array_pos, unsorted);
+  auto duration = duration_cast<microseconds>(stop - start);
+
+  std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
+
+  test_bucket_sort(sorted_array_neg, unsorted);
+
+  test_bucket_sort_worst_case();
+
+  test_bucket_sort_average_case();
+
+  test_bucket_sort_best_case();
 
   return 0;
 }
